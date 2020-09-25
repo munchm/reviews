a87880503cafb9c2c97a005137067d75c54521ff
Christopher Sorta <chris.sorta@yahoo.com>
2020-09-24 19:30:49 -0700

    Add aws functionality to access S3 bucket for photos

--- server/index.js
+++ server/index.js
@@ -2,7 +2,27 @@ const express = require('express');
 const app = express();
 const port = 5555;
 const db = require('../database');
+var AWS = require('aws-sdk');
 
+
+AWS.config.loadFromPath('./config.json');
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+// Create the parameters for calling listObjects
+var bucketParams = {
+  Bucket: 'photoreviews',
+};
+
+s3.listObjects(bucketParams, function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Contents);
+    console.log(data.Contents.length);
+  }
+});
 
 app.use(express.static(__dirname + '/../client/dist'));
 
