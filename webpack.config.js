0972a73f13e44b79009907d88d04662a3e08ff53
Christopher Sorta <chris.sorta@yahoo.com>
2020-09-28 12:59:24 -0700

    Configure project to run jest and enzyme tests

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,18 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        loader: 'babel-loader',
-        options: {
-          'presets': ['@babel/preset-env', '@babel/preset-react']
+        exclude: /node_modules/,
+        use: {
+
+          loader: 'babel-loader',
+          options: {
+            'presets': ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css']
   }
 };
