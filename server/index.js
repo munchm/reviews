30f7ecd09e593225502c5ca1547a44f229b47b09
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-10-26 16:46:23 -0700

    Implement node clusters

--- server/index.js
+++ server/index.js
@@ -1,71 +1,71 @@
 require('newrelic');
+
 const express = require('express');
-const app = express();
-const port = 5555;
 const db = require('../database/postgres');
 const bodyParser = require('body-parser');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-app.use(express.static(__dirname + '/../client/dist'));
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    // create worker
+    cluster.fork();
+  }
+} else {
+  const app = express();
+  const port = 5555;
+  app.use(express.static(__dirname + '/../client/dist'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+  // parse application/json
+  app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`Listening to reviews server on port ${port}`);
-});
-
-// legacy request
-app.get('/api/reviews/:id', (req, res) => {
-  db.getReviews(req.query.text, req.params.id, (err, data) => {
-    if (err) {
-      res.status(401).send();
-    } else {
-      res.status(200).send(data);
-    }
+  app.listen(port, () => {
+    console.log(`Listening to reviews server on port ${port}`);
   });
-});
 
-// get request for businesses
-app.get('/api/business/:businessId/reviews', (req, res) => {
-  const businessId = req.params.businessId;
-  db.getBusinessReviews(businessId, (err, data) => {
-    if (err) {
-      res.status(400).send('could not retrieve data');
-    } else {
-      res.status(200).send(data.rows);
-    }
+  // get request for businesses
+  app.get('/api/business/:businessId/reviews', (req, res) => {
+    const businessId = req.params.businessId;
+    db.getBusinessReviews(businessId, (err, data) => {
+      if (err) {
+        res.status(400).send('could not retrieve data');
+      } else {
+        res.status(200).send(data.rows);
+      }
+    });
   });
-});
 
-// get request for users
-app.get('/api/user/:userId/reviews', (req, res) => {
-  const userId = req.params.userId;
-  db.getUserReviews(userId, (err, data) => {
-    if (err) {
-      res.status(400).send('could not retrieve data');
-    } else {
-      res.status(200).send(data.rows);
-    }
+  // get request for users
+  app.get('/api/user/:userId/reviews', (req, res) => {
+    const userId = req.params.userId;
+    db.getUserReviews(userId, (err, data) => {
+      if (err) {
+        res.status(400).send('could not retrieve data');
+      } else {
+        res.status(200).send(data.rows);
+      }
+    });
   });
-});
 
-// post request for creating reviews
-app.post('/api/business/:businessId/users/:userId/review', (req, res) => {
-  const review = req.body;
-  review.userId = req.params.userId;
-  review.businessId = req.params.businessId;
-  db.createReview(review, (err, data) => {
-    if (err) {
-      res.status(400).send('could not retrieve data');
-    } else {
-      res.status(201).send('review added');
-    }
+  // post request for creating reviews
+  app.post('/api/business/:businessId/users/:userId/review', (req, res) => {
+    const review = req.body;
+    review.userId = req.params.userId;
+    review.businessId = req.params.businessId;
+    db.createReview(review, (err, data) => {
+      if (err) {
+        res.status(400).send('could not retrieve data');
+      } else {
+        res.status(201).send('review added');
+      }
+    });
   });
-});
+}
 
-app.get('/test', (req, res) => {
-  res.status(200).send('Test');
+cluster.on('exit', (worker, code, signal) => {
+  console.log('Worker %d died with code/signal %s.', worker.process.pid, signal || code);
+  cluster.fork();
 });
